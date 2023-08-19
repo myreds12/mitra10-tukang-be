5625b1a2a0abd6bbc06abdceb795c3a759498352
abimanyusatria665@gmail.com <abimanyusatria665@gmail.com>
2023-08-19 20:03:47 +0700

    adding auto register and user_roles to employee create

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
@@ -7,11 +17,11 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth/jwt-auth.guard';
 @Controller('employee')
 @UseGuards(JwtAuthGuard)
 export class EmployeeController {
-  constructor(private readonly employeeService: EmployeeService) { }
+  constructor(private readonly employeeService: EmployeeService) {}
 
   @Post('/create')
   create(@Body() createEmployeeDto: CreateEmployeeDto, @Request() req) {
-    const user_id = req.user.id
+    const user_id = req.user.id;
     return this.employeeService.create(createEmployeeDto, user_id);
   }
 
@@ -26,14 +36,18 @@ export class EmployeeController {
   }
 
   @Patch('/update/:id')
-  update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto, @Request() req) {
-    const user_id = req.user.id
+  update(
+    @Param('id') id: string,
+    @Body() updateEmployeeDto: UpdateEmployeeDto,
+    @Request() req,
+  ) {
+    const user_id = req.user.id;
     return this.employeeService.update(+id, updateEmployeeDto, user_id);
   }
 
   @Delete('/delete/:id')
   remove(@Param('id') id: string, @Request() req) {
-    const user_id = req.user.id
+    const user_id = req.user.id;
     return this.employeeService.remove(+id, user_id);
   }
 }
--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -2,14 +2,15 @@ import { Injectable, HttpStatus } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { hash } from 'bcrypt';
 import { createApiPropertyDecorator } from '@nestjs/swagger/dist/decorators/api-property.decorator';
+import { connect } from 'http2';
 
 @Injectable()
 export class EmployeeService {
-  constructor(private readonly dbService: PrismaService) { }
+  constructor(private readonly dbService: PrismaService) {}
   async create(createEmployeeDto: CreateEmployeeDto, user_id: number) {
     try {
-
       let full_name = createEmployeeDto.first_name;
 
       if (createEmployeeDto.middle_name) {
@@ -33,49 +34,72 @@ export class EmployeeService {
           whatsapp_number: createEmployeeDto.whatsapp_number,
           users: {
             connect: {
-              id: user_id
-            }
+              id: user_id,
+            },
           },
           store: {
             connect: {
-              id: createEmployeeDto.store_id
-            }
+              id: createEmployeeDto.store_id,
+            },
           },
           positions: {
             connect: {
-              id: createEmployeeDto.position_id
-            }
+              id: createEmployeeDto.position_id,
+            },
           },
-          created_by: user_id
-        }
-      })
-
+          created_by: user_id,
+        },
+      });
+      const user = await this.dbService.users.create({
+        data: {
+          username: `${employee.first_name + employee.last_name}165`,
+          password: await hash('tukanginwebsite165', 10),
+        },
+      });
+      const create_user_roles = await this.dbService.user_roles.create({
+        data: {
+          users: {
+            connect: { id: user.id }, // Assuming user_id is the ID of the user you're connecting
+          },
+          roles: {
+            connect: { id: 1 }, // Assuming 1 is the ID of the role you're connecting
+          },
+        },
+      });
+      const user_data = await this.dbService.user_roles.findUnique({
+        where: { id: create_user_roles.id },
+        include: { users: true, roles: true },
+      });
       return {
+        data: {
+          employee: employee,
+          user_data: user_data,
+        },
         status: HttpStatus.CREATED,
-        message: 'Successfully to Create Data'
-      }
+        message: 'Successfully to Create Data',
+      };
     } catch (error) {
       return {
         status: HttpStatus.BAD_REQUEST,
-        message: 'Failed to Create Data'
-      }
+        message: 'Failed to Create Data',
+      };
     }
   }
 
   async findAll() {
     try {
-      const employee = await this.dbService.employee.findMany()
+      const employee = await this.dbService.employee.findMany();
 
       return {
         status: HttpStatus.OK,
         message: 'Successfully to Get Data',
-        data: employee
-      }
+        data: employee,
+      };
     } catch (error) {
       return {
         status: HttpStatus.BAD_REQUEST,
-        message: 'Failed to Get Data'
-      }
+        message: 'Failed to Get Data',
+      };
     }
   }
 
@@ -83,26 +107,29 @@ export class EmployeeService {
     try {
       const employee = await this.dbService.employee.findFirst({
         where: {
-          id
-        }
-      })
+          id,
+        },
+      });
 
       return {
         status: HttpStatus.OK,
         message: 'Successfully to Find Data',
-        data: employee
-      }
+        data: employee,
+      };
     } catch (error) {
       return {
         status: HttpStatus.BAD_REQUEST,
-        message: 'Failed to Find Data'
-      }
+        message: 'Failed to Find Data',
+      };
     }
   }
 
-  async update(id: number, updateEmployeeDto: UpdateEmployeeDto, user_id: number) {
+  async update(
+    id: number,
+    updateEmployeeDto: UpdateEmployeeDto,
+    user_id: number,
+  ) {
     try {
-
       let full_name = updateEmployeeDto.first_name;
 
       if (updateEmployeeDto.middle_name) {
@@ -114,7 +141,7 @@ export class EmployeeService {
       }
       const employee = await this.dbService.employee.update({
         where: {
-          id
+          id,
         },
         data: {
           first_name: updateEmployeeDto.first_name,
@@ -129,33 +156,33 @@ export class EmployeeService {
           whatsapp_number: updateEmployeeDto.whatsapp_number,
           users: {
             connect: {
-              id: user_id
-            }
+              id: user_id,
+            },
           },
           store: {
             connect: {
-              id: updateEmployeeDto.store_id
-            }
+              id: updateEmployeeDto.store_id,
+            },
           },
           positions: {
             connect: {
-              id: updateEmployeeDto.position_id
-            }
+              id: updateEmployeeDto.position_id,
+            },
           },
           updated_at: new Date(),
-          updated_by: user_id
-        }
-      })
+          updated_by: user_id,
+        },
+      });
 
       return {
         status: HttpStatus.CREATED,
-        message: 'Successfully to Update Data'
-      }
+        message: 'Successfully to Update Data',
+      };
     } catch (error) {
       return {
         status: HttpStatus.BAD_REQUEST,
-        message: 'Failed to Update Data'
-      }
+        message: 'Failed to Update Data',
+      };
     }
   }
 
@@ -163,23 +190,23 @@ export class EmployeeService {
     try {
       const employee = await this.dbService.employee.update({
         where: {
-          id
+          id,
         },
         data: {
           deleted_at: new Date(),
           deleted_by: user_id,
-        }
-      })
+        },
+      });
 
       return {
         status: HttpStatus.OK,
-        message: 'Successfully to Delete Data'
-      }
+        message: 'Successfully to Delete Data',
+      };
     } catch (error) {
       return {
         status: HttpStatus.BAD_REQUEST,
-        message: 'Failed to Delete Data'
-      }
+        message: 'Failed to Delete Data',
+      };
     }
   }
 }
