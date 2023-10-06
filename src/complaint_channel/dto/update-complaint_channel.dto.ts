881ff90e320ee3d4fcbe521160a54157d98c6ad3
Alif_04 <alifnaufalh@gmail.com>
2023-10-06 17:44:37 +0700

    delete: multiple folder

--- src/complaint_channel/dto/update-complaint_channel.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateComplaintChannelDto } from './create-complaint_channel.dto';
-
-export class UpdateComplaintChannelDto extends PartialType(CreateComplaintChannelDto) {}
--- src/user_menu_permission/user_menu_permission.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserMenuPermissionService } from './user_menu_permission.service';
-import { UserMenuPermissionController } from './user_menu_permission.controller';
-
-@Module({
-  controllers: [UserMenuPermissionController],
-  providers: [UserMenuPermissionService],
-})
-export class UserMenuPermissionModule {}
