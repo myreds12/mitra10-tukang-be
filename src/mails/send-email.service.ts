0575803b9857449efd2bfe87b789693f0ffe1756
Alif_04 <alifnaufalh@gmail.com>
2024-02-03 22:44:06 +0700

    update: email order

--- src/mails/send-email.service.ts
+++ src/mails/send-email.service.ts
@@ -2,24 +2,60 @@ import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { OrderService } from 'src/order/order.service';
 import * as pug from 'pug';
+import * as fs from 'fs';
+import * as pdfkit from 'pdfkit';
+import 'jspdf-autotable';
 import { PrismaService } from 'src/prisma/prisma.service';
+import path from 'path';
 
 @Injectable()
 export class SendEmailService {
   constructor(private readonly mailerService: MailerService, private readonly orderService: OrderService, private readonly dbService: PrismaService) { }
 
+  async generatePDF(data: any): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+        const pdfStream = new pdfkit();
+        const buffers: Buffer[] = [];
+
+        pdfStream.on('data', (buffer) => {
+            buffers.push(buffer);
+        });
+
+        pdfStream.on('end', () => {
+            resolve(Buffer.concat(buffers));
+        });
+
+        pdfStream.on('error', (error) => {
+            reject(error);
+        });
+
+        pdfStream.pipe(fs.createWriteStream('uploads/pdf/')); 
+        pdfStream.end();
+    });
+}
+
   async sendMail(order_id: number) {
     const data = await this.orderService.findOne(order_id)
-    const mail = await this.mailerService.sendMail({
+    const generatePdf = await this.generatePDF(data)
+
+    await this.mailerService.sendMail({
       to: data.members.email, // list of receivers
       from: 'kreasisawalanusantara@gmail.com', // sender address
       subject: 'Email Order', // Subject line
       template: 'order',
       context: data,
       html: pug.renderFile('templates/index.pug', { data }),
+      attachments: [
+        {
+            filename: 'order.pdf',
+            content: generatePdf,
+            encoding: 'base64',
+            cid: 'unique@kreasisawalanusantara.com', // Ganti dengan CID yang unik
+        },
+    ],
     });
 
-    console.log('SUCCESS');
+
   }
 
   async sendMailResetPassword(user_id: number) {
@@ -55,6 +91,5 @@ export class SendEmailService {
       context: data,
       html: pug.renderFile('templates/reset-password.pug', { data }),
     });
-    return { message: "Success" };
   }
 }
