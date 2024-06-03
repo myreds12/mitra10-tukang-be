ca1ec4afaac24e388251db3ff9c4ff2a13f0a83b
ImFarel <sayafarel@gmail.com>
2024-06-03 15:15:37 +0000

    chore

--- src/incentive/dto/create-incentive.dto.ts
+++ src/incentive/dto/create-incentive.dto.ts
@@ -1,6 +1,5 @@
 import { Type } from 'class-transformer';
 import {
-  IsIn,
   IsNotEmpty,
   IsNumber,
   IsString,
@@ -8,24 +7,26 @@ import {
   Validate,
   ValidatorConstraintInterface,
   ValidatorConstraint,
+  IsEnum,
 } from 'class-validator';
+import { IncentiveType } from './incentive-type.enum';
 
 @ValidatorConstraint({ name: 'incentiveValidator', async: false })
 class IncentiveValidator implements ValidatorConstraintInterface {
   validate(value: number, args: ValidationArguments) {
     const type = (args.object as any).type;
 
-    if (type === 1 && value < 5000) return false;
-    if (type === 2 && (value <= 0 || value >= 100)) return false;
+    if (type === 2 && value < 5000) return false;
+    if (type === 1 && (value <= 0 || value >= 100)) return false;
     return true;
   }
   defaultMessage(args: ValidationArguments) {
     const type = (args.object as any).type;
     const value = (args.object as any).incentive;
 
-    if (type === 1 && value < 5000)
+    if (type === 2 && value < 5000)
       return 'The incentive should be bigger than 5000';
-    if (type === 2 && (value <= 0 || value >= 100))
+    if (type === 1 && (value <= 0 || value >= 100))
       return 'The incentive cannot be under 0 and bigger than 100';
   }
 }
@@ -46,8 +47,8 @@ export class CreateIncentiveDto {
   incentive: number;
 
   @IsNotEmpty()
-  @IsIn([1, 2])
-  type: number;
+  @IsEnum(IncentiveType)
+  type: IncentiveType;
 
   @IsNotEmpty()
   stores: number[];
