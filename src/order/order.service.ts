2976298d9989c2e0a2fc465369cab7a41116b5fb
ImFarel <sayafarel@gmail.com>
2024-10-07 17:37:02 +0700

    refactor: Improve validation for existing receipt number in OrderService

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -65,17 +65,17 @@ export class OrderService {
         },
       });
 
-      if(createOrderDto.receipt_number){
-        const existingOrder = await this.dbService.orders.findFirst({
-          where: { receipt_number: createOrderDto.receipt_number },
-        });
+      // if(createOrderDto.receipt_number){
+      //   const existingOrder = await this.dbService.orders.findFirst({
+      //     where: { receipt_number: createOrderDto.receipt_number },
+      //   });
     
-        if (existingOrder) {
-          throw new BadRequestException(
-            `Receipt number ${createOrderDto.receipt_number} already exists!`,
-          );
-        }
-      }
+      //   if (existingOrder) {
+      //     throw new BadRequestException(
+      //       `Receipt number ${createOrderDto.receipt_number} already exists!`,
+      //     );
+      //   }
+      // }
 
       // if(salesUser.sales.length === 0) throw new BadRequestException('Sales not found!');
 
@@ -1145,17 +1145,17 @@ export class OrderService {
         },
       });
 
-      if(updateOrderDto.receipt_number){
-        const existingOrder = await this.dbService.orders.findFirst({
-          where: { id: {not: id}, receipt_number: updateOrderDto.receipt_number },
-        });
+      // if(updateOrderDto.receipt_number){
+      //   const existingOrder = await this.dbService.orders.findFirst({
+      //     where: { id: {not: id}, receipt_number: updateOrderDto.receipt_number },
+      //   });
     
-        if (existingOrder) {
-          throw new BadRequestException(
-            `Receipt number ${updateOrderDto.receipt_number} already exists!`,
-          );
-        }
-      }
+      //   if (existingOrder) {
+      //     throw new BadRequestException(
+      //       `Receipt number ${updateOrderDto.receipt_number} already exists!`,
+      //     );
+      //   }
+      // }
       const files: Array<Prisma.order_filesCreateManyOrderInput> =
         order_files.map((item) => ({
           type: 'any',
