c009432fc2aaa605c9e694aac043a3a3e23d93e6
Alif_04 <alifnaufalh@gmail.com>
2025-02-27 10:33:16 +0700

    chore: comment unessecary code

--- src/invoices/invoices.controller.ts
+++ src/invoices/invoices.controller.ts
@@ -30,10 +30,10 @@ import { RequestWithUser } from 'src/common/interface/request-with-user.interfac
 export class InvoicesController {
   constructor(private readonly invoicesService: InvoicesService) { }
 
-  @Get('/order')
-  async getOrderInvoice(@Query() query: QueryParamsDto) {
-    return await this.invoicesService.getOrderInvoice(query);
-  }
+  // @Get('/order')
+  // async getOrderInvoice(@Query() query: QueryParamsDto) {
+  //   return await this.invoicesService.getOrderInvoice(query);
+  // }
 
   @Get('/:id/export-excel')
   async invoiceDetailsExportExcel(
